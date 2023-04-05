d5fb70098da5f9f2b9fab39d04fabe4d56333c6a
frankligomez <frago1978@gmail.com>
2023-04-05 01:35:12 -0500

    Ajuste_05042023_2

---
+++ java/tema 4/estacion/tema_4_control/src/control.java
@@ -0,0 +1,19 @@
+public class control {
+
+    public static void main(String[] args) 
+    {
+        String estacion = "otoño";
+        
+        if (estacion == "primavera") 
+        {
+            System.out.println("Es primavera");
+        } 
+        else if (estacion == "verano")
+        {
+            System.out.println("Es verano");
+        }
+        else {
+            System.out.println("Es otra estación...");
+        }
+    }
+}
