857f4402cf33264fa0ce1b52477c5e200c033a62
frankligomez <frago1978@gmail.com>
2023-04-05 23:59:59 -0500

    Ajuste_05042023_3

---
+++ java/tema 4/while_do_while/tema_4_buqles/src/buqles.java
@@ -0,0 +1,12 @@
+public class buqles {
+    public static void main(String[] args) 
+    {
+        int contador = 10;
+
+        while (contador >0)
+        {
+            System.out.println(contador);
+            contador = contador - 1;
+        }
+    }
+}
