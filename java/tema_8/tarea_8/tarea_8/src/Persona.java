4f5c43141b79681ea4c6a98e3b5e54923b5e60f9
frankligomez <frago1978@gmail.com>
2023-04-10 11:45:21 -0500

    Avance_10042023

---
+++ java/tema_8/tarea_8/tarea_8/src/Persona.java
@@ -0,0 +1,49 @@
+public class Persona 
+{
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public int getEdad() 
+    { 
+        return edad; 
+    }
+
+    public void setEdad(int edad) 
+    {
+        this.edad = edad;
+    }
+
+    public String getNombre() 
+    { 
+        return nombre; 
+    }
+
+    public void setNombre(String nombre) 
+    {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() 
+    { 
+        return telefono; 
+    }
+
+    public void setTelefono(String telefono) 
+    {
+        this.telefono = telefono;
+    }
+}
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Persona persona = new Persona();
+        persona.setEdad(40);
+        persona.setNombre("Frankli");
+        persona.setTelefono("1234567890");
+        System.out.println("Edad: " + persona.getEdad());
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Teléfono: " + persona.getTelefono());
+    }
+}
